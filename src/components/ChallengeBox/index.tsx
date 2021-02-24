4537c30d26a4c4e7e4c26c1fe3aeeb03af33d13e
Artur Polo Norte <arturcastropolonorte@gmail.com>
2021-02-24 12:14:27 -0300

    :art: improve structure of challengebox active

--- src/components/ChallengeBox/index.tsx
+++ src/components/ChallengeBox/index.tsx
@@ -1,8 +1,30 @@
 import styles from '../../styles/components/ChallengeBox.module.css';
 
 export function ChallengeBox(){
+    const hasActiveChallenge = true;
+
     return(
         <div className={styles.challengeBoxContainer}>
+            {hasActiveChallenge 
+            ? (
+                <div className={styles.challengeBoxActive}>
+                    <header>Ganhe 300 xp</header>
+                    <main>
+                        <img src="icons/body.svg"/>
+                        <strong>Novo desafio</strong>
+                        <p>Levante e faca uma caminhada de 3 minutos</p>
+                    </main>
+                    <footer>
+                        <button type="button"
+                        className={styles.challengeSucceededButton}
+                        >Ganhei</button>
+                        <button type="button"
+                        className={styles.challengeFailedButton}
+                        >Falhei</button>
+                    </footer>
+                </div>
+            )
+            :(
             <div className={styles.challengeBoxNotActive}>
                 <strong>Finalize um ciclo para receber um desafio</strong>
                 <p>
@@ -10,6 +32,8 @@ export function ChallengeBox(){
                     Avance de level completando desafios.
                 </p>
             </div>
+            )
+            }
         </div>
     );
 }
